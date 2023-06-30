de3b4e59a7b8d4124d9234876faf40dd7931ca83
aleksejrevakin <alexey.amahasla@gmail.com>
2023-06-30 23:34:13 +0300

    Create project

---
+++ DelivertSwiftUI/DelivertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelivertSwiftUIApp.swift
+//  DelivertSwiftUI
+//
+//  Created by Алексей Ревякин on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DelivertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
